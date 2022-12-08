11510d7ae7cce0a25349a045301ad9e1d2595647
tylerthecat <71778585+tylerthecat@users.noreply.github.com>
2022-12-08 20:19:21 +0300

    Update tstt.js

--- tstt.js
+++ tstt.js
@@ -2,14 +2,14 @@ var express = require('express');
 const ejs = require('ejs');
 var tools = require('./app');
 const path = require('path');
-
+const PORT = process.env.PORT || 5050
 // Initialise Express
 var app = express();
 // Render static files
 app.use(express.static(path.join(__dirname, 'static')));
 // Port website will run o
 app.set('view engine', 'ejs');
-app.listen(3000,function(){
+app.listen(PORT,function(){
     console.log('Server start: http://localhost:8080/')
 });
 app.get('/', function (req, res) {
